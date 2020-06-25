3eca8af7b3ee9bccde933964fc7a7519a942d85b
Pixass <61846700+Pixass@users.noreply.github.com>
2020-06-25 09:47:56 +0100

    implement code that pulls in our shop dta from firestore it into the shop reducer from our shop page. Also included our thSpinner HOC

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -17,6 +17,9 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 
+
+
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -38,6 +41,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth );
+      
     });
   }
 
@@ -70,7 +74,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  
 });
 
 const mapDispatchToProps = dispatch => ({
--- crown-clothing/src/redux/user/user.selectors.js
+++ crown-clothing/src/redux/user/user.selectors.js
@@ -6,4 +6,4 @@ const selectUser = state => state.user;
 export const selectCurrentUser = createSelector(
   [selectUser],
   (user) => user.currentUser
-)
+);
