a0becd0599e2b596a0f9319eebedaf511e01538a
Pixass <61846700+Pixass@users.noreply.github.com>
2020-07-02 12:03:30 +0100

    created user and cart sagas signIn, sign up and sign in with google

--- crown-clothing/src/redux/store.js
+++ crown-clothing/src/redux/store.js
@@ -2,11 +2,16 @@ import {createStore , applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 
+import creatSagaMiddleware from 'redux-saga';
+
+import rootSaga from './root-saga';
+
 import rootReducer from './root-reducer';
 
 
+const sagaMiddleware = creatSagaMiddleware();
 
-const middlewares= [];
+const middlewares = [sagaMiddleware];
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -15,7 +20,11 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+
+sagaMiddleware.run(rootSaga);
+
+
 export const persistor = persistStore (store);
 
 
-export default {store , persistor};
+export default {store , persistStore};
